172146282f0c79d2e73d4e9e7d4dd8514bba448c
SanskarGour <sanskargour1234@gmail.com>
2024-03-28 11:14:27 +0530

    final change

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,26 +1,24 @@
-import React, { useContext } from "react";
+import React from "react";
 import Navbar from "./Navbar";
 import HomeMain from "./HomeMain";
 import Projects from "./Projects";
 import Skills from "./Skills";
 import Contact from "./Contact";
 import Footer from "./Footer";
-import { AppContext } from "./context/AppContext";
 import Sidebar from "./Sidebar";
 
 function Home(){
-  const { sidebar, setSidebar } = useContext(AppContext);
   
   return(
     <div className="pt-6 relative min-w-[100vw] w-[100%] min-h-[100vh] h-[100%]">
       <div className="translucent-bg"></div>
-      {/* <Navbar /> */}
-      {/* <Sidebar /> */}
-      {/* <HomeMain /> */}
-      {/* <Projects /> */}
-      {/* <Skills /> */}
+      <Navbar />
+      <Sidebar />
+      <HomeMain />
+      <Projects />
+      <Skills />
       <Contact />
-      {/* <Footer /> */}
+      <Footer />
     </div>
   );
 }
